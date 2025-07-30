253f49e357683ff0f55d2ee0da59f04c25f26af9
Andrew Scott Pitblado <apitblado@gmail.com>
2025-07-30 13:28:00 -0400

    Added sticky positioning to header

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
+    <header className="bg-white sticky top-0 z-50 shadow">
       <nav
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
